d524803c1ac0789cb9a10a4574ba3f94ba8647f2
teeeeeeemo <binooooo@naver.com>
2017-02-13 12:00:53 +0900

    mysite->index수정

---
+++ src/com/bit2017/mysite/controller/MainServlet.java
@@ -0,0 +1,31 @@
+package com.bit2017.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bit2017.mysite.web.main.MainActionFactory;
+import com.bit2017.web.Action;
+
+@WebServlet("/main")
+public class MainServlet  extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+		Action action = new MainActionFactory().getAction(actionName);
+		action.execute(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/bit2017/web/ActionFactory.java
@@ -0,0 +1,8 @@
+package com.bit2017.web;
+
+public abstract class ActionFactory{
+	
+	abstract public Action getAction(String actionName);
+	
+
+}
